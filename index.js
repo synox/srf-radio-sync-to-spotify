ae33bb31f4a7be7719597898dec5c8a3014aa6b0
synox <synox>
2021-01-02 17:36:40 +0100

    cleanup

--- index.js
+++ index.js
@@ -3,41 +3,25 @@ const spotify = require("./spotify.js")
 let spotifyCredentials = require('./spotify_credentials.json');
 
 
-async function main(channel, fromDate, toDate) {
-
+async function main() {
     // spotify.startLoginWorkflow(spotifyCredentials);
     spotify.login(spotifyCredentials)
 
-    const songs = await srf.getPlaylist(channel, fromDate, toDate)
-    console.log(songs)
+    await copyToSpotify(srf.channelIds.srfRadio3, "2021-01-01")
+}
+
+async function copyToSpotify(channel, fromDate) {
+    const name = `SRF 3 ${fromDate}`
+    if (spotify.userPlaylistExists(name)) {
+        console.log("playlist already exists, stopped")
+        return;
+    }
+
+    const songs = await srf.getPlaylist(channel, fromDate, fromDate)
 
     const spotifyTrackIds = await spotify.findSongs(songs)
-    console.log(spotifyTrackIds)
 
-    const name = `SRF 3 ${fromDate}`
-    // if ( spotify.userPlaylistExists(name) ) {
-    //     console.log("playlist already exists, stopped")
-    //     return;
-    // }
     spotify.createPlaylist(name, spotifyTrackIds);
 }
 
-main(srf.channelIds.srfRadio3, "2020-12-29", "2020-12-29");
-
-
-function askOnce(question) {
-    const stdin = process.stdin, stdout = process.stdout
-
-    stdin.resume();
-    stdout.write(question + ": ");
-
-    return new Promise(res => {
-        stdin.once('data', function (data) {
-            res(data.toString().trim());
-        });
-    });
-}
-
-
-
-
+main();
